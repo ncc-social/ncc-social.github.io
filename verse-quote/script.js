dc848f308122417349b5b3b5ebe3417c1bfeccc5
ncc-social <social@ncc.gov.gh>
2024-03-05 12:08:49 +0000

    fixed translation

--- verse-quote/script.js
+++ verse-quote/script.js
@@ -1,9 +1,10 @@
 // API endpoint with random verse
-const apiUrl = `https://api.quran.com/api/v4/verses/random?translations=171&fields=text_uthmani,chapter_id`;
+const apiUrl = `https://api.quran.com/api/v4/verses/random?translations=131&fields=text_uthmani,chapter_id`;
 
 fetch(apiUrl)
   .then(response => response.json())
   .then(data => {
+    console.log(data);
     // Display verse and translation
     document.getElementById("verse").innerText = data.verse.text_uthmani;
     document.getElementById("translation").innerText = data.verse.translations[0].text;
